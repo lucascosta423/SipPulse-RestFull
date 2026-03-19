20af296ad708ae2c3bd234ca31a88568843ccb0e
lucassantos <lukascosta.ls17@gmail.com>
2026-03-19 11:11:52 -0300

    feat(account-group): add adapter, service and WS config

---
+++ src/main/java/com/sippulse/soapadapter/config/AccountGroupWSConfig.java
@@ -0,0 +1,42 @@
+package com.sippulse.soapadapter.config;
+
+import com.sippulse.soapadapter.client.accountGroupWS.AccountGroupWS;
+import com.sippulse.soapadapter.client.accountGroupWS.SipPulse;
+import com.sippulse.soapadapter.sippulse.config.SoapProperties;
+import jakarta.xml.ws.BindingProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URL;
+
+@Configuration
+public class AccountGroupWSConfig {
+
+    @Bean
+    public AccountGroupWS accountGroupWS(SoapProperties properties) {
+        try {
+            String baseUrl = properties.getEndpoints().get("account_group");
+
+            URL wsdlUrl = new URL(baseUrl + "?wsdl");
+
+            SipPulse service = new SipPulse(wsdlUrl);
+
+            AccountGroupWS port = service.getAccountGroupWSPort();
+
+            configure((BindingProvider) port, baseUrl);
+
+            return port;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao criar AccountGroupWS", e);
+        }
+    }
+
+    private void configure(BindingProvider bp, String baseUrl) {
+        bp.getRequestContext().put(
+                BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                baseUrl
+        );
+        bp.getRequestContext().put("com.sun.xml.ws.connect.timeout", 5000);
+        bp.getRequestContext().put("com.sun.xml.ws.request.timeout", 10000);
+    }
+}
