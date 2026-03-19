3fe4b02aebb5efb8ac56a67ac422581639b3dd89
lucassantos <lukascosta.ls17@gmail.com>
2026-03-19 16:23:37 -0300

    feat(reload): add WS config

---
+++ src/main/java/com/sippulse/soapadapter/config/ReloadWSConfig.java
@@ -0,0 +1,44 @@
+package com.sippulse.soapadapter.config;
+
+import com.sippulse.soapadapter.client.reloadWS.ReloadModulesWS;
+import com.sippulse.soapadapter.client.reloadWS.SipPulse;
+import com.sippulse.soapadapter.sippulse.config.SoapProperties;
+import jakarta.xml.ws.BindingProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URL;
+
+@Configuration
+public class ReloadWSConfig {
+
+        @Bean
+        public ReloadModulesWS reloadModulesWS(SoapProperties properties) {
+            try {
+                String baseUrl = properties.getEndpoints().get("did");
+
+                URL wsdlUrl = new URL(baseUrl + "?wsdl");
+
+                SipPulse service = new SipPulse(wsdlUrl);
+
+                ReloadModulesWS port = service.getReloadModulesWSPort();
+
+                configure((BindingProvider) port, baseUrl);
+
+                return port;
+
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao criar ReloadModulesWS", e);
+            }
+        }
+
+        private void configure(BindingProvider bp, String baseUrl) {
+            bp.getRequestContext().put(
+                    BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                    baseUrl
+            );
+            bp.getRequestContext().put("com.sun.xml.ws.connect.timeout", 5000);
+            bp.getRequestContext().put("com.sun.xml.ws.request.timeout", 10000);
+        }
+
+}
