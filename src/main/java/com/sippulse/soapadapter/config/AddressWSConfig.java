ed57d80b90bce49933a9c7bd656f5c7ff178ad55
lucassantos <lukascosta.ls17@gmail.com>
2026-03-19 11:21:24 -0300

    feat(address): add WS config

---
+++ src/main/java/com/sippulse/soapadapter/config/AddressWSConfig.java
@@ -0,0 +1,44 @@
+package com.sippulse.soapadapter.config;
+
+import com.sippulse.soapadapter.client.addressWS.AddressWS;
+import com.sippulse.soapadapter.client.addressWS.SipPulse;
+import com.sippulse.soapadapter.sippulse.config.SoapProperties;
+import jakarta.xml.ws.BindingProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URL;
+
+@Configuration
+public class AddressWSConfig {
+
+        @Bean
+        public AddressWS addressWS(SoapProperties properties) {
+            try {
+                String baseUrl = properties.getEndpoints().get("address");
+
+                URL wsdlUrl = new URL(baseUrl + "?wsdl");
+
+                SipPulse service = new SipPulse(wsdlUrl);
+
+                AddressWS port = service.getAddressWSPort();
+
+                configure((BindingProvider) port, baseUrl);
+
+                return port;
+
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao criar AddressWS", e);
+            }
+        }
+
+        private void configure(BindingProvider bp, String baseUrl) {
+            bp.getRequestContext().put(
+                    BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                    baseUrl
+            );
+            bp.getRequestContext().put("com.sun.xml.ws.connect.timeout", 5000);
+            bp.getRequestContext().put("com.sun.xml.ws.request.timeout", 10000);
+        }
+
+}
