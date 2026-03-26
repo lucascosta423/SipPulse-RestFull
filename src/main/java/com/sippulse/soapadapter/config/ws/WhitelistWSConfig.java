e8826f76fb60fdbfc2e65c0e1ec9cb263c5827c6
lucassantos <lukascosta.ls17@gmail.com>
2026-03-26 17:16:12 -0300

    refactor(config): utilize SoapClientFactory in WhitelistWSConfig
    - Refactor WhitelistWSConfig to use SoapClientFactory for client creation.
    - Remove manual SOAP client configuration and handling logic.

--- src/main/java/com/sippulse/soapadapter/config/ws/WhitelistWSConfig.java
+++ src/main/java/com/sippulse/soapadapter/config/ws/WhitelistWSConfig.java
@@ -2,42 +2,20 @@ package com.sippulse.soapadapter.config.ws;
 
 import com.sippulse.soapadapter.client.whitelistWS.SipPulse;
 import com.sippulse.soapadapter.client.whitelistWS.WhiteListWS;
-
 import com.sippulse.soapadapter.config.soap.properties.SoapProperties;
-import jakarta.xml.ws.BindingProvider;
+import com.sippulse.soapadapter.factory.SoapClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.net.URL;
 
 @Configuration
 public class WhitelistWSConfig {
 
     @Bean
-    public WhiteListWS whiteListWS(SoapProperties properties) {
-        try {
-            String baseUrl = properties.getEndpoints().getWhitelist();
-
-            URL wsdlUrl = new URL(baseUrl + "?wsdl");
-
-            SipPulse service = new SipPulse(wsdlUrl);
-
-            WhiteListWS port = service.getWhiteListWSPort();
-
-            configure((BindingProvider) port, baseUrl);
-
-            return port;
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao criar WhiteListWS", e);
-        }
-    }
-
-    private void configure(BindingProvider bp, String baseUrl) {
-        bp.getRequestContext().put(
-                BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-                baseUrl
+    public WhiteListWS whiteListWS(SoapProperties properties, SoapClientFactory factory) {
+        return factory.createClient(
+                properties.getEndpoints().getWhitelist(),
+                SipPulse::new,
+                SipPulse::getWhiteListWSPort
         );
-        bp.getRequestContext().put("com.sun.xml.ws.connect.timeout", 5000);
-        bp.getRequestContext().put("com.sun.xml.ws.request.timeout", 10000);
     }
 }
